3cce243aec2256881f06c8ebd1a1408a92699268
Alfiya-dev <alfiya@kisanlink.in>
2026-01-21 13:40:12 +0530

    remove ssl

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,27 +1,27 @@
-// import { PrismaClient } from '../generated/client/client';
-// import { PrismaPg } from '@prisma/adapter-pg';
-
-// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-// const prisma = new PrismaClient({ adapter });
-
-// export default prisma;
-
-import * as fs from 'fs';
 import { PrismaClient } from '../generated/client/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const sslCertPath = process.env.DB_SSL_CERT_PATH;
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-  ssl: sslCertPath
-    ? {
-        ca: fs.readFileSync(sslCertPath, 'utf-8'),
-        rejectUnauthorized: true,
-      }
-    : undefined,
-});
-
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
+
+// import * as fs from 'fs';
+// import { PrismaClient } from '../generated/client/client';
+// import { PrismaPg } from '@prisma/adapter-pg';
+
+// const sslCertPath = process.env.DB_SSL_CERT_PATH;
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: sslCertPath
+//     ? {
+//         ca: fs.readFileSync(sslCertPath, 'utf-8'),
+//         rejectUnauthorized: true,
+//       }
+//     : undefined,
+// });
+
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
