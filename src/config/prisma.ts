7648c810b16070935d5eca9f89d6fa4c271bddd7
Alfiya-dev <alfiya@kisanlink.in>
2026-01-21 14:09:09 +0530

    minor changes

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,8 +1,20 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import { PrismaClient } from '../generated/client/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const adapter = new PrismaPg({ 
-  connectionString: process.env.DATABASE_URL 
+const sslCertPath = process.env.DB_SSL_CERT_PATH;
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+  ssl: sslCertPath
+    ? {
+        ca: fs.readFileSync(path.resolve(sslCertPath), 'utf-8'),
+        rejectUnauthorized: false,
+      }
+    : {
+        rejectUnauthorized: false,
+      },
 });
 
 const prisma = new PrismaClient({ adapter });
