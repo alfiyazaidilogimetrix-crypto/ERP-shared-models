cb2ff0f56c3a946800aba769dad3161f361af62c
Alfiya-dev <alfiya@kisanlink.in>
2026-01-21 14:14:33 +0530

    minor changes

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,22 +1,30 @@
-import * as fs from 'fs';
-import * as path from 'path';
+// import * as fs from 'fs';
+// import * as path from 'path';
+// import { PrismaClient } from '../generated/client/client';
+// import { PrismaPg } from '@prisma/adapter-pg';
+
+// const sslCertPath = process.env.DB_SSL_CERT_PATH;
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: sslCertPath
+//     ? {
+//         ca: fs.readFileSync(path.resolve(sslCertPath), 'utf-8'),
+//         rejectUnauthorized: false,
+//       }
+//     : {
+//         rejectUnauthorized: false,
+//       },
+// });
+
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
+
 import { PrismaClient } from '../generated/client/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const sslCertPath = process.env.DB_SSL_CERT_PATH;
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-  ssl: sslCertPath
-    ? {
-        ca: fs.readFileSync(path.resolve(sslCertPath), 'utf-8'),
-        rejectUnauthorized: false,
-      }
-    : {
-        rejectUnauthorized: false,
-      },
-});
-
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
